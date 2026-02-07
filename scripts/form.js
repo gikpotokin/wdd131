b88ac758023ab5cf6d2e7953a741e584e1ab3274
Godswill Ikpotokin <godswill@ikpotokin.ng>
2026-02-07 21:49:40 +0100

    product review
    created product review

---
+++ scripts/form.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const productSelect = document.querySelector("#product");
+
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+});
